2908c8986d1bd4e84402e8f8b6928070e4112248
Gabriel de Oliveira Maciel <macielgabriel2310@gmail.com>
2026-04-10 16:57:22 -0400

    Initial Commit

---
+++ MusicPlayerTests/MusicPlayerTests.swift
@@ -0,0 +1,19 @@
+//
+//  MusicPlayerTests.swift
+//  MusicPlayerTests
+//
+//  Created by Gabriel Maciel on 10/04/26.
+//
+
+import Testing
+@testable import MusicPlayer
+
+struct MusicPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
